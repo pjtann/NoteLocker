7f15010f99646c1104840ec587509e37e38d1d6b
PT <PT@PTs-MacBook-Pro.local>
2016-04-12 19:48:29 -0600

    NoteLocker (BlocNotes) 1 Introduction Completed

---
+++ com.pjtann.NoteLocker/com.pjtann.NoteLocker/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  com.pjtann.NoteLocker
+//
+//  Created by PT on 4/12/16.
+//  Copyright © 2016 PeterTanner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ com.pjtann.NoteLocker/com.pjtann.NoteLocker/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  com.pjtann.NoteLocker
+//
+//  Created by PT on 4/12/16.
+//  Copyright © 2016 PeterTanner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
